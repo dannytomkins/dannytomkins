8558ead63db4e4cd6386a82b0ec75c83c9b221cb
Danny Tomkins <tomkinsdan@gmail.com>
2023-11-26 10:23:09 -0600

    various styling and articleItem component updates

--- src/components/ArticleItem/ArticleItem.js
+++ src/components/ArticleItem/ArticleItem.js
@@ -1,14 +1,38 @@
-import './ArticleItem.css'
+import React, { useRef } from 'react';
+import './ArticleItem.css';
 
 const ArticleItem = ({ article }) => {
-  return (
-    <div className='article'>
-      <h2>{article.title}</h2>
-      <h5>{article.date}</h5>
-      <a href={article.url} target="_blank" rel="noopener noreferrer">{article.url}</a>
-      <p>{article.body}</p>
-    </div>
-  )
-}
+  const articleRef = useRef(null);
 
-export default ArticleItem
+  const backgroundStyle = {
+    background: `url(${article.img}) center/cover no-repeat`,
+  };
+
+  const bodyStyle = {
+    marginBottom: "10px"
+  }
+
+  const renderBodyWithLineBreaks = () => {
+    return article.body.split('\n').map((paragraph, index) => (
+      <p key={index} style={bodyStyle}>{paragraph}</p>
+    ));
+  };
+
+  return (
+    <div className='article' ref={articleRef} style={backgroundStyle}>
+      <div className='text-overlay'>
+        <h2>{article.title}</h2>
+        <h5>{article.date}</h5>
+        <br />
+        <a href={article.url} target='_blank' rel='noopener noreferrer'>
+          {article.url}
+        </a>
+        <br />
+        <br />
+        {renderBodyWithLineBreaks()}
+      </div>
+    </div>
+  );
+};
+
+export default ArticleItem;
